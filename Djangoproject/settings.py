287ed6c41ddeed84077233947ff77810f969bf1f
ARUNNG2004 <arun1990govind@gmail.com>
2024-10-22 06:42:47 +0530

    first commi

--- Djangoproject/settings.py
+++ Djangoproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-p0q8)@53u7vb6u6!*72kzt7t)z@c42ih_o2n#906=seuzuvn2j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mydjangoworld.onrender.com' ,'127.0.0.1']
 
 
 # Application definition
